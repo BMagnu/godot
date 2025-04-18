7ef1230348d993bd31d5bb80471004b29f3f29b5
Birk Magnussen <6238428+BMagnu@users.noreply.github.com>
2025-04-18 10:25:49 +0200

    Silence overzealous error print

--- modules/gdscript/gdscript_lambda_callable.cpp
+++ modules/gdscript/gdscript_lambda_callable.cpp
@@ -105,7 +105,8 @@ void GDScriptLambdaCallable::call(const Variant **p_arguments, int p_argcount, V
 				bool was_freed = false;
 				captures[i].get_validated_object_with_check(was_freed);
 				if (was_freed) {
-					ERR_PRINT(vformat(R"(Lambda capture at index %d was freed. Passed "null" instead.)", i));
+					//This is intended behaviour, don't print an error here
+					//ERR_PRINT(vformat(R"(Lambda capture at index %d was freed. Passed "null" instead.)", i));
 					static Variant nil;
 					args.write[i] = &nil;
 				}
